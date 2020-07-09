2d9286658904d30a638c0bc52bf99b479e5a2c6f
Jeanette Payne <jeanetterpayne@gmail.com>
2020-07-09 01:52:56 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,7 +12,7 @@ end
 
 def add_element_to_start_of_array(array, element)
   ["Hello", "Bonjour"] 
-  array.unshift("Hola")
+  array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
