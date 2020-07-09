de6caee3f800c9b6dfec9f2c83991319096a0735
Jeanette Payne <jeanetterpayne@gmail.com>
2020-07-09 01:56:57 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -31,7 +31,8 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  
+  ["Today", "Tomorrow", "Yesterday", "Every Day", "Never"]
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
